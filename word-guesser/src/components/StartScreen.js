db741de29218670d866d39a88c022075766a533f
Carlos Papaleo <carlospapaleo2@gmail.com>
2022-08-18 22:11:13 -0300

    Start Screen Finished
    Start Screen finished.

---
+++ word-guesser/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Word Guesser</h1>
+      <p>Click on the button to start!</p>
+      <button>Begin!</button>
+    </div>
+  );
+};
+
+export default StartScreen;
